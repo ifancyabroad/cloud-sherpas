57cafedecea07f63e1429a33715d19da8b6a1a9a
ifancyabroad <edgar.nightingale@fanhubmedia.com>
2020-11-08 15:49:01 +0000

    Add customer carousel

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.customers-carousel').slick({
+        autoplay: true,
+        arrows: false,
+        slidesToShow: 8,
+        variableWidth: true
+    });
+});
